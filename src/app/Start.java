674178e0a3d9ada60845adb2285c70667f96b76a
504 <504@DESKTOP-0MVNHLQ>
2022-03-30 11:47:50 +0900

    21일차

--- src/app/Start.java
+++ src/app/Start.java
@@ -1,5 +1,27 @@
 package app;
 
-public class Start {
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
+public class Start extends Application{
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		// 스테이지 로고 설정ㄷ
+			// 1. 이미지 불러오기
+		Image image = new Image("img/a.png");
+		
+		stage.setTitle("Ezen"); // 2. 스테이지 창 이름
+		stage.show(); // 스테이지 열기
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		launch(args);
+	}
+	
+	
 }
