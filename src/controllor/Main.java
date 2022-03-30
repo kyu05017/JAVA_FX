dec936e8f1565829d8a8dfa392f49cb14410bf87
504 <504@DESKTOP-0MVNHLQ>
2022-03-30 13:04:57 +0900

    21일차

--- src/controllor/Main.java
+++ src/controllor/Main.java
@@ -1,6 +1,5 @@
 package controllor;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -17,18 +16,18 @@ public class Main implements Initializable{
     
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		System.out.println("Main뷰가 실행 되었습니다.");
-		// 1. boarderpane 
+			// 1. boarderpane 
 		loadpage("/view/login");
+		System.out.println("Main뷰가 실행 되었습니다.");
+	
 	}
 	
-	public void loadpage(String page) {
+	public void loadpage( String page ) {
 		try {
-			Parent parent = FXMLLoader.load( getClass().getResource(page+".fxml"));
-			boarderPane.setCenter(parent);
-		} catch (IOException e) {
-			System.out.println("페이지 연결 실패");
-		}
+			// 페이지(fxml) 객체화 
+			Parent parent = FXMLLoader.load( getClass().getResource(page+".fxml") ); // 무조건 예외처리
+			boarderPane.setCenter(parent); // 컨테이너(fxml) 가운데에 페이지 넣기 
+		}catch( Exception e ) { System.out.println("페이지 연결 실패");}
 		
 	}
 }
