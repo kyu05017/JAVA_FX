5a207e8efe24a9b744c2a81dc5310a5c7c871f28
504 <504@DESKTOP-0MVNHLQ>
2022-03-30 12:02:36 +0900

    21일차

--- src/app/Start.java
+++ src/app/Start.java
@@ -11,10 +11,18 @@ public class Start extends Application{
 		
 		// 스테이지 로고 설정ㄷ
 			// 1. 이미지 불러오기
-		Image image = new Image("img/a.png");
+		Image image = new Image("img/a.png"); // 상대 경로
+		//Image image2 = new Image("//C:\\Users\\504\\git\\JAVA_FX\\src\\img"); 절대경로
+			// 절대 경로 VS 상대 경로
+			// 절대 경로 : 모든 경로
+				// C:\Users\504\git\JAVA_FX\src\img
+			// 상대 경로 : 현위치 [ 프로젝트 기준 src ] 기준 경로
+				// 생략 [ src폴더부터 ] img/파일명.확장자
 			// 2. 스테이지 설정
 		stage.getIcons().add(image);
-		stage.setTitle("Ezen"); // 2. 스테이지 창 이름
+		stage.setResizable(false);	// 3. 스테이지 크기 고정
+		
+		stage.setTitle("햄버거 먹고싶은 사람의 FX프로그래밍"); // 2. 스테이지 창 이름
 		stage.show(); // 스테이지 열기
 	}
 	
