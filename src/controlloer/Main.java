03c4d0f7809de6b1c86533d901332f725db0beb3
504 <504@DESKTOP-0MVNHLQ>
2022-03-30 12:11:31 +0900

    21일차

--- src/controlloer/Main.java
+++
@@ -1,5 +0,0 @@
-package controlloer;
-
-public class Main {
-
-}
---
+++ src/controllor/Main.java
@@ -0,0 +1,15 @@
+package controllor;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class Main implements Initializable{
+				// Initializable : view가 새로 열렸을때 초기값 설정 메소드 제공
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		System.out.println("Main뷰가 실행 되었습니다.");
+		
+	}
+}
