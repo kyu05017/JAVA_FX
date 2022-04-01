68bf28a472d0df9d0bca3f3aa3d50c3a4fa220a0
504 <504@DESKTOP-0MVNHLQ>
2022-04-01 12:29:40 +0900

    22일차

--- src/Aapplication/Day22/DB.java
+++ src/Aapplication/Day22/DB.java
@@ -1,13 +1,18 @@
 package Aapplication.Day22;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class DB {
-	
+public class DB implements Initializable{
+			// Initializable : fxml이 열렸을때 초기값 [ 처음값 ] 설정 메소드 제공
     @FXML
     private TextField txtwriter;
 
@@ -26,11 +31,49 @@ public class DB {
     @FXML
     void get(ActionEvent event) {
     	System.out.println("DB내 데이터를 호출합니다.");
+    	
+    	//1. DB 연동 객체 만들기
+    	Day22_2 dbcon = new Day22_2();
+    	// 2. DB 객체내 메소드 호출 후 리스트로 받기
+    	ArrayList<Data> datalist =dbcon.get();
+    	// 3. 리스트를 컨트롤에 넣어주기
+    	for(Data temp : datalist) {
+    		// txtcontentlist.appendText : 해당 컨트롤에 내용 추가
+    		txtcontentlist.appendText(
+				temp.getNumber()+". " +
+				temp.getWriter()+" : "+
+				temp.getContents()+"\n"
+			);
+    	}
     }
 
     @FXML
     void write(ActionEvent event) {
     	System.out.println("DB내 데이터를 저장합니다.");
+    	
+    	String writer = txtwriter.getText(); 
+    	String content = txtcontent.getText(); 
+    	// 1. DB 얀덩 객체 만들기
+    	Day22_2 dbcon = new Day22_2();
+    	boolean result = dbcon.write(writer, content);
+    	if(result) {
+    		System.out.println("DB 저장 성공");
+    		
+    		// 성공시 컨트롤에 입력된 데이터 지워주기 
+    			// txtid명.setText() : 해당 컨트롤에 값 넣기
+    		txtwriter.setText("");
+    		txtcontent.setText("");
+    	}
+    	else {
+    		System.out.println("DB 저장 실패");
+    	}
+    }
+    
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) { // 초기값 [ 처음값 ] 설정 메소드
+    	
+    	Day22_2 day22_2 = new Day22_2(); // 생성자에 DB 연동 코드를 넣었기 때문에 객체 선언시 연동
+    	
     }
 
 
--- src/Aapplication/Day22/Day22_2.java
+++ src/Aapplication/Day22/Day22_2.java
@@ -1,9 +1,104 @@
 package Aapplication.Day22;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import com.mysql.cj.protocol.Resultset;
+import com.mysql.cj.xdevapi.PreparableStatement;
+
 public class Day22_2 {
 
+	// 1. 필드
+		// 연동 객체
+	private Connection connection; // DB 연동객체
+	// 2. 생성자
+	public Day22_2() {
+		
+		try { // 자바외 연결시 무조건 일반 예외 발생 
+			// 1. DB 드라이브 클래스 호출 [ DB 회사 마다 다르기 때문에 암기X 정리O ]
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			// 2. DB 연결
+			connection = DriverManager.getConnection(
+					// JDBC : JAVA DATABASE CONNECTE
+					// jdbc:mysql://IP주소( 로컬[본인PC] 이면 localhost):port번호/DB이름?시간설정
+					// , 계정명 , 비밀번호
+					"jdbc:mysql://localhost:3306/javafx?serverTimezone=UTC",
+					"root","1234");
+			System.out.println("DB연동 성공");
+		}
+		catch(Exception e) {
+			System.out.println("DB연동 실패");
+		}
+	}
 	
+	// 3. 메소드
 	
+	// 1. 삽입
+	public boolean write(String writer, String content) {
+		
+		try {
+		// 1. SQL 작성 [ DB내 테이블에 데이터 삽입 ]
+			// db에 테이블에 데이터 저장 : insert into 테이블명1 ( 필드명1 , 필드명2 ) values ( 필드명1의 값 , 필드명2의 값) 
+		String sql = "insert into test(writer,content) values(?,?)";
+		// 2. SQL 설정 [ 연결된 DB에 SQL 설정 ] 
+			// PreparableStatement : 연결된 DB에ㅐ서 테이블 조작 ( 삽입, 삭제 수정 생성 ) 할때 쓰는 인터페이스
+		PreparedStatement ps = connection.prepareStatement(sql);
+		ps.setString(1 , writer);	// sql내 작성한 첫번째 ? 에 변수 넣기 [ 1 : 첫번째 ]
+		ps.setString(2 , content);	//  [ 2 : 두번째 ]
+		
+		// 3. sql 실행
+		ps.executeUpdate(); // sql 삽입
+		
+		// 성공시 true
+		return true;
+		}
+		catch (Exception e) {
+			System.out.println("[sql 연결 실패] : 사유 " + e);
+		}
+		
+		// 실패시 false
+		return false;
+	}
 	
+	// 2. 데이터 호출
+	public ArrayList<Data> get() {
+		
+		try {
+			// *
+			ArrayList<Data> datelsit = new ArrayList<>();
+			
+			// 1. sql  작성 [ 데이터 호출 ]
+				//select * (모든 필드) from 테이블명
+			String sql = "select * from test";
+			
+			// 2. SQL 조작 [ DB 와 연결된 객체와 조작ㄱ인터페이스 연결 ]
+			PreparedStatement ps = connection.prepareStatement(sql);
+			
+			// 3. sql 실행 [ ResultSet 인터페이스 java.sql 패키지 ]
+			ResultSet rs =  ps.executeQuery();
+			
+			// * 결과물 하나가 아니고 여러개 이므로 반복문 사용해서
+				// 한줄씩[레코드] 객체화 -> 리스트에 저장
+			while(rs.next()) {
+				// 1. 한줄식 [ 레코드 ] 단위 객체화
+				Data temp = new Data(
+					rs.getInt(1), // 해당 줄 [ 레코드 ]의 첫번째 필드 [ 세로 ] 값을 가져오기
+					rs.getString(2), // 해당 줄 [ 레코드 ]의 첫번째 필드 [ 세로 ] 값을 가져오기
+					rs.getString(3)); // 해당 줄 [ 레코드 ]의 첫번째 필드 [ 세로 ] 값을 가져오기
+				datelsit.add(temp);
+			}
+			// 반복문이 종료되면 리스트 반환
+			// 성공시 데이터 목록 반환
+			return datelsit;
+		}
+		catch (Exception e) {
+			System.out.println("[sql 연결 실패] : 사유 " + e);
+		}
+		// 실패시 
+		return null;
+	}
 	
 }
