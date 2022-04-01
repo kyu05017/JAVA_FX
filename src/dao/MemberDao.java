47711c0895bc22e9df9dde864654ae192e6b648c
504 <504@DESKTOP-0MVNHLQ>
2022-04-01 13:52:33 +0900

    22일차

--- src/dao/MemberDao.java
+++ src/dao/MemberDao.java
@@ -1,5 +1,49 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import dto.Member;
+
 public class MemberDao {	// DB 접근 객체
 
+	private Connection con; // SB 연동시 사용되는 클래스 : DB 연동클래스
+	private PreparedStatement ps; // 연결된 DB내 SQL 조작 할때 사용하는 인터페이스 : DB 조작
+	private ResultSet rs; // 검색 [ select ]
+	
+	// * DB 연동할때마다 객체 선언시 불필요한 코드를 방지
+	
+	private static MemberDao dao; // DB 연동 객체;
+	
+	public MemberDao() {
+		try {
+			// DB 연동 
+			Class.forName("com.mysql.cj.jdbc.Driver");// 1. DB 드라이버 가져오기
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/javafx?serverTimezone=UTC","root","1234"); // 2. DB 주소 연결
+			dao = this;
+			System.out.println("DB연동 성공");
+		} catch (Exception e) {
+			System.out.println("DB연동 실패");
+		}
+
+	}
+	// 메소드
+		// 1. 회원가입 메소드 ( 인수를 DB에 넣을 아이디 비밀번호 이메일 주소 )
+	public boolean signup(Member member){
+		return false;
+	}
+		// 2. 로그인 메소드 ( 로그인에 필요한 아이디 비밀번호 )
+	public boolean login(String id, String pw) {
+		return false;
+	}
+		// 3. 아이디 찾기 메소드 ( 아이디 찾기에 필요한 이메일 )
+	public String findid(String email) {
+		return null;
+	}
+		// 4. 비밀번호 찾기 메소드 ( 비밀번호 찾기에 필요한 아이디 이메일 ) 
+	public String findpw(String id ,String email) {
+		return null;
+	}
 }
--- src/dto/Member.java
+++ src/dto/Member.java
@@ -1,5 +1,82 @@
 package dto;
 
 public class Member {	// 데이터 모델
+	
+	private int m_num; // 회원번호
+	private String m_id;
+	private String m_pw;
+	private String m_email;
+	private String m_address;
+	private int m_point;
+	private String m_since;
+	
+	public Member() {}
 
+	public Member(int m_num, String m_id, String m_pw, String m_email, String m_address, int m_point, String m_since) {
+		this.m_num = m_num;
+		this.m_id = m_id;
+		this.m_pw = m_pw;
+		this.m_email = m_email;
+		this.m_address = m_address;
+		this.m_point = m_point;
+		this.m_since = m_since;
+	}
+
+	public int getM_num() {
+		return m_num;
+	}
+
+	public void setM_num(int m_num) {
+		this.m_num = m_num;
+	}
+
+	public String getM_id() {
+		return m_id;
+	}
+
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+
+	public String getM_pw() {
+		return m_pw;
+	}
+
+	public void setM_pw(String m_pw) {
+		this.m_pw = m_pw;
+	}
+
+	public String getM_email() {
+		return m_email;
+	}
+
+	public void setM_email(String m_email) {
+		this.m_email = m_email;
+	}
+
+	public String getM_address() {
+		return m_address;
+	}
+
+	public void setM_address(String m_address) {
+		this.m_address = m_address;
+	}
+
+	public int getM_point() {
+		return m_point;
+	}
+
+	public void setM_point(int m_point) {
+		this.m_point = m_point;
+	}
+
+	public String getM_since() {
+		return m_since;
+	}
+
+	public void setM_since(String m_since) {
+		this.m_since = m_since;
+	}
+	
+	
 }
