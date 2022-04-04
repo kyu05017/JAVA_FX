0bdf3246aed08e5a3798016606c6abc2a2866a72
504 <504@DESKTOP-0MVNHLQ>
2022-04-04 16:37:25 +0900

    23일차

--- src/controllor/home/Changeinfo.java
+++ src/controllor/home/Changeinfo.java
@@ -37,7 +37,7 @@ public class Changeinfo implements Initializable{
     	// 1. 컨트롤에 입력된 값 가져오기
     	String add = txtadd.getText(); // 해당 fxid에 입력된 값 가져오디
     	String email = txtemail.getText(); // 해당 fxid에 입력된 값 가져오디
-    	
+    	Alert alert = new Alert(AlertType.INFORMATION);
    	
     	// 2. db 객체내 메소드 호출
     	boolean result = MemberDao.dao.changeInfo(Login.member.getM_num(),add, email);
@@ -45,7 +45,6 @@ public class Changeinfo implements Initializable{
     	if(result) {
     		// 메세지 출력
     		System.out.println("수정완료");
-    		Alert alert = new Alert(AlertType.INFORMATION);
     		alert.setTitle("회원정보 수정");
     		alert.setHeaderText("이메일과 주소가 수정되었습니다. [ 다시 로그인 하세요. ]");
     		alert.setContentText("완료");
@@ -56,7 +55,6 @@ public class Changeinfo implements Initializable{
     	}
     	else {
     		System.out.println("수정실패");
-    		Alert alert = new Alert(AlertType.INFORMATION);
     		alert.setTitle("회원정보 수정");
     		alert.setHeaderText("수정 실패. [ 관리자 문의 ]");
     		alert.setContentText("완료");
