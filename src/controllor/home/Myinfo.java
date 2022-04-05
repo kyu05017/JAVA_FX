e846d1557b792bb43a504609ca9c4ac3087690fd
kyu05 <kyu05@121.139.17.105>
2022-04-06 05:15:02 +0900

    25일차

--- src/controllor/home/Myinfo.java
+++ src/controllor/home/Myinfo.java
@@ -7,6 +7,10 @@ import controllor.login.Login;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.util.Duration;
 
 public class Myinfo implements Initializable{
 
@@ -24,10 +28,28 @@ public class Myinfo implements Initializable{
 
     @FXML
     private Label my_email;
+    
+    @FXML
+    private MediaView homeview;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-    	
+    	Media media = new Media(getClass().getResource("/img/login5.mp4").toString());
+		// 2. 미디어플레이어 객체에 동영상 넣기
+		MediaPlayer mediaPlayer = new MediaPlayer(media);
+			// 3. 미이더 플레이어에 미디어 넣기
+		homeview.setMediaPlayer(mediaPlayer); 
+		// 무한반복
+		mediaPlayer.setOnEndOfMedia(new Runnable() {
+			
+			@Override
+			public void run() {
+				mediaPlayer.seek(Duration.ZERO);
+				
+			}
+		});
+		// 미디어 플레이어 시작
+		mediaPlayer.play();
     	my_id.setText("ID : "+Login.member.getM_id());
     	my_email.setText("이메일 : "+Login.member.getM_email());
     	my_add.setText("주소 : "+Login.member.getM_address());
