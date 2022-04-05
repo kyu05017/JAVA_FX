c7860e064d5c7a33a68847b9f4402be1f9c4f60e
504 <504@DESKTOP-0MVNHLQ>
2022-04-05 10:18:47 +0900

    24일차

--- src/dao/BoardDao.java
+++ src/dao/BoardDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import dto.Board;
 
 public class BoardDao {
 	
@@ -11,7 +12,7 @@ public class BoardDao {
 	private PreparedStatement ps; // 2. 연결된 DB내 SQL 조작 할때 사용하는 인터페이스 : DB 조작
 	private ResultSet rs; // 3. sql 결과 
 	
-	public static BoardDao dao = new BoardDao();
+	public static BoardDao dao = new BoardDao();	// db 연동 객체
 	
 	public BoardDao() { // 4. 생성자에서 연동하는 이유 : 객체 생성시 바로 연동
 		try {
@@ -22,5 +23,21 @@ public class BoardDao {
 		catch (Exception e) {System.out.println("보드 DB연동 실패" + e);}
 	}
 	
-	
+	// 메소드
+	//1. 글쓰기
+	public boolean write(Board board) {
+		return false;
+	}
+	// 2. 글목록
+	public boolean boardlist() {
+		return false;
+	}
+	// 3. 글삭제
+	public boolean delete(int b_num) {
+		return false;
+	}
+	// 4. 글수정
+	public boolean updaete(int b_num, String title, String contents) {
+		return false;
+	}
 }
