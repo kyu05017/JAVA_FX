ece4035cb19cb92514fe43ded95664a0a87370ab
504 <504@DESKTOP-0MVNHLQ>
2022-04-05 11:31:54 +0900

    24일차

--- src/controllor/board/BoardWrite.java
+++ src/controllor/board/BoardWrite.java
@@ -9,34 +9,31 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 
 public class BoardWrite implements Initializable{
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	@FXML
     private Button btback;
 
     @FXML
-    private TextField txttitle;
-
-    @FXML
-    private TextArea txtcontents;
+    private TextArea btwrtie;
 
     @FXML
     private Button btwrite1;
 
     @FXML
-    void addback(ActionEvent event) {
+    void accback(ActionEvent event) {
     	Home.home.loadpage("/view/board/board");
     }
 
     @FXML
     void addwrite(ActionEvent event) {
 
-    }
-    @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
-    	// TODO Auto-generated method stub
-    	
     }
 }
