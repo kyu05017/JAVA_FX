802942d5130cd4c3c88f7e88d92e715bb1a1a2ce
kyu05 <kyu05@121.139.17.105>
2022-04-06 21:01:57 +0900

    25일차

--- src/app/Start.java
+++ src/app/Start.java
@@ -8,7 +8,7 @@ import javafx.scene.image.Image;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-public class Start extends Application{
+public class Start extends Application{ // 20220406 09 01
 
 	@Override
 	public void start(Stage stage) throws Exception {
