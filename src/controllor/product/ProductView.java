7d759cd12c1f706b27f93bc2546b7f5862419834
504 <504@DESKTOP-0MVNHLQ>
2022-04-06 16:12:21 +0900

    25일차

--- src/controllor/product/ProductView.java
+++ src/controllor/product/ProductView.java
@@ -1,8 +1,12 @@
 package controllor.product;
 
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 
+import controllor.home.Home;
+import dao.ProductDao;
+import dto.Product;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -10,6 +14,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class ProductView implements Initializable{
@@ -45,12 +50,12 @@ public class ProductView implements Initializable{
 
     @FXML
     void accback(ActionEvent event) {
-
+    	Home.home.loadpage("/view/product/product");
     }
 
     @FXML
     void accdelete(ActionEvent event) {
-
+    	
     }
 
     @FXML
@@ -59,6 +64,15 @@ public class ProductView implements Initializable{
     }
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
+    	Product product = ProductControl.select;
+    	
+    	img.setImage(new Image(product.getP_img()) );
+    	txtpname.setText(product.getP_name());
+    	txtpcontents.setText(product.getP_contents());
+    	txtdate.setText(product.getP_date());
+    	DecimalFormat df2 = new DecimalFormat("#,##0¿ø");
+    	String aa = df2.format(product.getP_money());
+    	txtprise.setText(aa);
     	
     }
 }
