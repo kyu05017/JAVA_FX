b3f11befbe10f8242e0d38b024f2eb4156eae484
504 <504@DESKTOP-0MVNHLQ>
2022-04-06 12:05:21 +0900

    25일차

--- src/controllor/product/ProductAdd.java
+++ src/controllor/product/ProductAdd.java
@@ -1,8 +1,8 @@
 package controllor.product;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import controllor.home.Home;
 import controllor.login.Login;
 import dao.ProductDao;
@@ -18,9 +18,15 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
 
 public class ProductAdd implements Initializable{
+	
+	private String pimage = null;
 	
 	@FXML
     private Button btback;
@@ -63,8 +69,10 @@ public class ProductAdd implements Initializable{
 
     @FXML
     void accadd(ActionEvent event) {
+    	Alert alert = new Alert(AlertType.INFORMATION);
     	// 1. 컨트롤에 입력된 데이터 받기
     	String name = txtpname.getText();
+    	
     	String prise = txtpprise.getText();
     	int new_prise = Integer.parseInt(prise);
     	String contents = txtpcontents.getText();
@@ -85,15 +93,28 @@ public class ProductAdd implements Initializable{
     	}
     	int m_num = Login.member.getM_num();
     	// 2. 유효성 검사
-    	
+    	if(txtpname.equals("")) {
+    		alert.setHeaderText("제목을 입력해 주세요.");
+    		alert.showAndWait();
+    		Home.home.loadpage("/view/product/product");
+    	}
+    	if(txtpprise.equals("")) {
+    		alert.setHeaderText("가격을 입력해 주세요.");
+    		alert.showAndWait();
+    		Home.home.loadpage("/view/product/product");
+    	}
+    	if(txtpcontents.equals("")) {
+    		alert.setHeaderText("내용을 입력해 주세요.");
+    		alert.showAndWait();
+    		Home.home.loadpage("/view/product/product");
+    	}
     	// 3. 객체화
-    	Product product = new Product(0, name, null, contents, category, new_prise, 1, null, m_num);
+    	Product product = new Product(0, name, pimage, contents, category, new_prise, 1, null, m_num);
     	
     	// 4. DB처리
     	boolean result = ProductDao.dao.addproduct(product);
     	// 5. 결과러리
     	if(result) {
-    		Alert alert = new Alert(AlertType.INFORMATION);
     		alert.setHeaderText("제품이 등록 되었습니다.");
     		alert.showAndWait();
     		Home.home.loadpage("/view/product/product");
@@ -104,10 +125,35 @@ public class ProductAdd implements Initializable{
     void accback(ActionEvent event) {
     	Home.home.loadpage("/view/product/product");
     }	
-
+    
+    
+    
     @FXML
-    void imgadd(ActionEvent event) {
-
+    void imgadd(ActionEvent event) {// 이미지 등록 버튼을 클릭했을때
+    	// 1. 파일 선택 클래스
+    	FileChooser chooser = new FileChooser(); 
+    	
+    	// 2. 파일 선택 [ 형식 필터링 ]
+    	chooser.getExtensionFilters().add(new ExtensionFilter("이미지파일 : image file", "*png","*jepg","*gif","*jpg"));
+    	
+    	// 3. 새로운 stage에서 파일 선택 화면 실행 
+    	File file = chooser.showOpenDialog( new Stage() );
+    		// 파일 선택 객체 .showOpenDialog( 스테이지 이름 );
+    		// FileChooser 화면에서 선택된 파일을 file 클래스 객체화
+    	
+    	// 4. 선택한 파일의 경로
+    	txtpath.setText("이미지 경로 : "+file.getPath());
+    	
+    	// 5. 파일경로 
+    	pimage = file.toURI().toString();
+    	
+    	System.out.println(file.toURI()); // 경로 구분선 : /
+    	System.out.println(file.getPath()); // 경로 구분선  : \
+    	System.out.println(file.toURI().toString());  // toString 문자열 반환
+    	
+    	// 5. 이미지 미리보기 컨트롤에 이미지 띄우기
+    	Image image = new Image(pimage); // 해당 이미지 경로 값이 / 로 구분 되어야 함
+    	img.setImage(image);
     }
     
     @Override
