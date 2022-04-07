d7ccd93dd2a82d3718ad45159b00ceea77b4d422
504 <504@DESKTOP-0MVNHLQ>
2022-04-07 13:43:02 +0900

    26일차

--- src/Aapplication/Day26/DAy26_server.java
+++ src/Aapplication/Day26/DAy26_server.java
@@ -2,6 +2,7 @@ package Aapplication.Day26;
 
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class DAy26_server {
 	
@@ -21,12 +22,17 @@ public class DAy26_server {
 		
 		try {
 			// 1. 서버 소켓 만들기
-			ServerSocket socket = new ServerSocket();
+			ServerSocket serverSocket = new ServerSocket();
 			// 2. 서버 소켓 바인딩
-			socket.bind(new InetSocketAddress("127.168.102.5",5000));
+			serverSocket.bind(new InetSocketAddress("127.168.102.5",5000));
 			// 3. 클라이언트의 요청 대기
 			while(true) {
 				System.out.println("[ 서버가 연결 대기중 입니다. ]");
+				// 4. 요청이 있을경우 수락
+				Socket socket = serverSocket.accept();
+				// 5. 수락된 소켓의 정보 확인
+				InetSocketAddress inetSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
+				System.out.println("[ 클라이언트와 연결이 되었습니다. 연결된 정보 "+ inetSocketAddress + " ]");
 			}
 		}
 		catch(Exception e) {
--- src/Aapplication/Day26/Day26_client.java
+++ src/Aapplication/Day26/Day26_client.java
@@ -1,5 +1,26 @@
 package Aapplication.Day26;
 
-public class Day26_client {
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 
+public class Day26_client {
+	
+	public static void main(String[] args) {
+		
+		// 1. 소켓 만둘기
+		Socket socket = new Socket();
+		
+		// 2. 서버 소켓으로 연결하기 [ 연결할 아이피와 포트번호를 입력 ]
+		try {
+			while(true) {
+				socket.connect(new InetSocketAddress("127.168.102.5",5000));
+				System.out.println("[ 서버 연결 성공 ]");
+				
+			} 
+		}
+		catch (IOException e) {
+		}
+	}
+	
 }
