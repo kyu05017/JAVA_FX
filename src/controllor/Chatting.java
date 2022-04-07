4f49524ffe5a8e2a0e27524e1709778f466829bb
kyu05 <kyu05@14.51.93.139>
2022-04-07 19:57:24 +0900

    26일차

--- src/controllor/Chatting.java
+++ src/controllor/Chatting.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import controllor.login.Login;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -14,111 +15,117 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class Chatting implements Initializable{
-	@FXML
-    private TextArea txtcontents;
+public class Chatting implements Initializable {
 
     @FXML
-    private Button btcon;
+    private Button btnconnect;
 
     @FXML
-    private TextField txt;
+    private TextArea txtcontent;
 
     @FXML
-    private Button btsend;
+    private TextField txtmsg;
+
+    @FXML
+    private Button btnsend;
 
     @FXML
     private TextField txtname;
 
     @FXML
-    private TextField txtipadd;
+    private TextField txtip;
 
     @FXML
     private TextField txtport;
     
-    Socket socket;
+    Socket socket;   // 1. 클라이언트 소켓 선언 
     
-    
+    // 2. 클라이언트 실행 메소드
     public void clientstart() {
     	Thread thread = new Thread() { // 멀티스레드 
     		@Override
     		public void run() {
     			try {
     				socket = new Socket("127.0.0.1",1234); // 서버의 ip와 포트번호 넣어주기 [ 서버와 연결 ]
-    				send( Login.member.getM_id()+"님 입장했습니다\n"); // 접속과 동시에 입장메시지 보내기 
+    				send(Login.member.getM_id()+"님 입장했습니다\n"); // 접속과 동시에 입장메시지 보내기 
     				receive(); // 접속과 동시에 받기 메소드는 무한루프
     			}catch(Exception e ) {}
     		};
     	};// 멀티스레드 구현 끝
     	thread.start(); // 멀티스레드 실행
     }
-	public void clientstop() {try {socket.close();}catch (Exception e) {System.out.println("클라이언트 닫기 실패 " + e);}}
-	public void send(String msg) {
-		
-		Thread thread = new Thread() {
-			@Override
-			public void run() {
-				try {
-				OutputStream outputStream = socket.getOutputStream(); // 1. 출력 스트림
-				outputStream.write(msg.getBytes()); // 2. 내보내기
-				outputStream.flush(); // 3. 스트림 초기화
-				}
-				catch (Exception e) {
-					System.out.println("클라이언스 보내기 오류 " + e);
-				}
-			}
-		};
-		thread.start();
-	}
-	public void receive() {
-		try {
-			while (true) {
-				InputStream inputStream = socket.getInputStream();
-				byte[] bytes = new byte[1024];
-				inputStream.read(bytes);
-				String msg = new String(bytes);
-				txtcontents.appendText(msg);
-			}
-		}
-		catch (Exception e) {
-			System.out.println("클라이언스 받기 오류 " + e);
-		}
-	}
+    // 3. 클라이언트 종료 메소드 
+    public void clientstop() {  try{ socket.close(); }catch(Exception e ) {} }
+    
+    // 4. 서버에게 메시지 보내기 메소드 
+    public void send( String msg ) {
+    	Thread thread = new Thread() { 
+    		@Override
+    		public void run() {
+    			try {
+    				OutputStream outputStream = socket.getOutputStream(); // 1. 출력 스트림
+    				outputStream.write( msg.getBytes() ); // 2. 내보내기
+    				outputStream.flush(); // 3. 스트림 초기화 [ 스트림 내 바이트 지우기 ]
+    			}catch( Exception e ) {} 
+    		}
+    	};// 멀티스레드 구현 끝 
+    	thread.start();
+    }
+    // 5. 서버에게 메시지 받기 메소드 
+    public void receive() {
+    	try {
+	    	while(true) {
+	    		InputStream inputStream = socket.getInputStream(); // 1. 입력 스트림
+	    		byte[] bytes = new byte[1000]; 	// 2. 바이트배열 선언 
+	    		inputStream.read(bytes);		// 3. 읽어오기 
+	    		String msg = new String(bytes);	// 4. 바이트열 -> 문자열 변환
+	    		txtcontent.appendText(msg); 	// 4. 받은 문자열을 메시지창에 띄우기 
+	    	}
+    	}catch( Exception e ) {}
+    }
     
     @FXML
-    void acsend(ActionEvent event) { // 전송 버튼 눌렀을때ㅑ
-    	String msg = txt.getText() + "\n";
-    	send(msg);
-    	txt.setText("");
-    	txt.requestFocus(); // 보내기후 메세지입력창으로 포커스 ( 커서 ) 이동
+    void connect(ActionEvent event) {
+    	if( btnconnect.getText().equals("채팅방 입장") ) {// 만약에 버튼의 텍스트가 "채팅방 입장" 이면 
+    		
+    		clientstart(); // 클라이언트 실행 메소드 
+    		
+    		txtcontent.appendText("---[채팅방 입장]---\n");
+    		btnconnect.setText("채팅방 나가기");
+    		
+    		txtmsg.setText("");				// 채팅입력창 빈칸으로 설정
+    		txtmsg.setDisable(false); 		// 채팅입력창 사용가능 
+        	txtcontent.setDisable(false); 	// 채팅창 목록 사용가능
+        	btnsend.setDisable(false); 		// 전송버튼 사용가능
+        	txtmsg.requestFocus();  		// 채팅입력창으로 포커스[커서] 이동
+    		
+    	}else {
+    		clientstop(); // 클라이언트 종료 메소드 
+    		
+    		txtcontent.appendText("---[채팅방 퇴장]---\n");
+    		btnconnect.setText("채팅방 입장");
+    		
+    		txtmsg.setText("채팅방 입장후 사용가능");
+        	txtmsg.setDisable(true); 		// 채팅입력창 사용금지 
+        	txtcontent.setDisable(true); 	// 채팅창 목록 사용금지
+        	btnsend.setDisable(true); 		// 전송버튼 사용금지
+    	}
     }
-
     @FXML
-    void actcon(ActionEvent event) {
-    	if(btcon.getText().equals("채팅방 입장")){
-    		clientstart();
-    		txtcontents.appendText("---채팅방 입장 ---\n");
-    		btcon.setText("채팅방 나가기");
-    		txt.setText("");
-    		txt.setDisable(false);	// 채팅입력창 잠금
-        	txtcontents.setDisable(false); // 채팅창 잠금
-        	btsend.setDisable(false); // 버특 사용 막음
-        	txt.requestFocus();
-    	}
-    	else {
-    		clientstop();
-    		txtcontents.appendText("---채팅방 퇴장 ---\n");
-    		btcon.setText("채팅방 입장");
-    		txt.setDisable(true);	// 채팅입력창 잠금
-        	txtcontents.setDisable(true); // 채팅창 잠금
-        	btsend.setDisable(true); // 버특 사용 막음
-    	}
+    void send(ActionEvent event) { // 전송 버튼을 눌렀을때
+    	String msg = txtmsg.getText()+"\n"; // 보낼 메시지
+    	send( msg ); // 메시지 보내기 
+    	txtmsg.setText(""); 	// 보내기 후 메시지입력창 지우기
+    	txtmsg.requestFocus();	// 보내기 후 메시지입력창으로 포커스(커서) 이동
     }
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-    	txt.setDisable(true);	// 채팅입력창 잠금
-    	txtcontents.setDisable(true); // 채팅창 잠금
-    	btsend.setDisable(true); // 버특 사용 막음
-    	txt.setText("채팅방 입장후 사용 가능합니다.");
+    	// 채팅fxml 열렸을때 초기값 메소드 
+    		// * 채팅방 입장전에 아래 fxid를 사용못하게 금지 
+    	txtmsg.setText("채팅방 입장후 사용가능");
+    	txtmsg.setDisable(true); 		// 채팅입력창 사용금지 
+    	txtcontent.setDisable(true); 	// 채팅창 목록 사용금지
+    	btnsend.setDisable(true); 		// 전송버튼 사용금지
     }
+	
 }
--- src/dto/Client.java
+++ src/dto/Client.java
@@ -4,9 +4,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
-import controllor.ServerControl;
-
-
+import controllor.Server;
 
 public class Client {
 	// 서버에 접속된 클라이언트의 클래스 
@@ -34,14 +32,14 @@ public class Client {
 						inputStream.read( bytes ); 		// 3. 입력스트림으로 바이트 읽어오기 
 						String msg = new String(bytes);	// 4. 바이트열 -> 문자열 변환
 						// * 서버가 받은 메시지를 현재 서버에 접속된 모든 클라이언트에게 받은 메시지 보내기
-						for( Client client   : ServerControl.clientlist ) {
+						for( Client client   : Server.clientlist ) {
 							client.send( msg ); // 받은 메시지를 서버에 접속된[ clientlist ] 모든 클라이언트에게 메시지 보내기
 						}
 					}
 				}catch( Exception e ) {}
 			}
 		}; // 멀티스레드 구현 끝 
-		ServerControl.threadpool.submit(runnable);  // 해당 멀티스레드를 스레드풀에 넣어주기 
+		Server.threadpool.submit(runnable);  // 해당 멀티스레드를 스레드풀에 넣어주기 
 	}
 	// 4. 서버가 메시지 보내는 메소드  [ 실행조건 : 서버가 메시지를 받았을때 ]
 	public void send( String msg ) { 
@@ -54,7 +52,7 @@ public class Client {
 				}catch( Exception e ) {}
 			}
 		}; // 멀티스레드  구현 끝 
-		ServerControl.threadpool.submit(runnable); // 해당 멀티스레드를 스레드풀에 넣어주기 
+		Server.threadpool.submit(runnable); // 해당 멀티스레드를 스레드풀에 넣어주기 
 	}
 	
 	
