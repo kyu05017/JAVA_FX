920f4e1397469cd826049bd2b0cf086a3b42500a
504 <504@DESKTOP-0MVNHLQ>
2022-04-07 15:31:21 +0900

    26일차

--- src/dto/Client.java
+++ src/dto/Client.java
@@ -1,5 +1,18 @@
 package dto;
 
-public class Client {
+import java.net.Socket;
 
+public class Client {
+	// 서버에서 등러오는 클라이언트의 클래스
+	
+	// 1. 소켓
+	Socket socket;
+	// 2. 생성자
+	public Client(Socket socket) {
+		this.socket = socket;
+	}
+	
+	// 3. 서버로 메세지를 받는 메소드
+	
+	// 4. 서버가 메세지를 보내는 메소드
 }
